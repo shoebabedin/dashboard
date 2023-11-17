186df174ab2de78a80a78f2a9ab190921db59208
shoeb abedin <shoebabedin12@gmail.com>
2023-11-17 23:51:40 +0600

    update

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
-import { useMediaQuery, Box, Container, Toolbar } from '@mui/material';
+import { useMediaQuery, Box, Toolbar } from '@mui/material';
 
 // project import
 import Drawer from './Drawer';
@@ -21,7 +21,7 @@ const MainLayout = () => {
   const theme = useTheme();
   const matchDownLG = useMediaQuery(theme.breakpoints.down('xl'));
 
-  const { container, miniDrawer } = useConfig();
+  const {  miniDrawer } = useConfig();
   const dispatch = useDispatch();
 
   const menu = useSelector((state) => state.menu);
@@ -54,27 +54,15 @@ const MainLayout = () => {
       <Drawer open={open} handleDrawerToggle={handleDrawerToggle} />
       <Box component="main" sx={{ width: 'calc(100% - 260px)', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
         <Toolbar />
-        {container && (
-          <Container
-            maxWidth="lg"
-            sx={{ px: { xs: 0, sm: 2 }, position: 'relative', minHeight: 'calc(100vh - 110px)', display: 'flex', flexDirection: 'column' }}
-          >
-            {/* <Breadcrumbs navigation={navigation} title titleBottom card={false} divider={false} /> */}
-            <div className="dash_main_conent_wrapper">
-              <Outlet />
-            </div>
-            {/* <Footer /> */}
-          </Container>
-        )}
-        {!container && (
-          <Box sx={{ position: 'relative', minHeight: 'calc(100vh - 110px)', display: 'flex', flexDirection: 'column' }}>
-            {/* <Breadcrumbs navigation={navigation} title titleBottom card={false} divider={false} /> */}
-            <div className="dash_main_conent_wrapper">
-              <Outlet />
-            </div>
-            {/* <Footer /> */}
-          </Box>
-        )}
+ 
+    
+    {/* <Breadcrumbs navigation={navigation} title titleBottom card={false} divider={false} /> */}
+    <div className="dash_main_conent_wrapper">
+      <Outlet />
+    </div>
+    {/* <Footer /> */}
+   
+      
       </Box>
     </Box>
   );
