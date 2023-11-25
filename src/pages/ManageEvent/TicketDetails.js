3acd34fa382b2eedfeb55d0c961e81673e92b49a
shoeb abedin <shoebabedin12@gmail.com>
2023-11-25 19:33:53 +0600

    update

--- src/pages/ManageEvent/TicketDetails.js
+++ src/pages/ManageEvent/TicketDetails.js
@@ -43,7 +43,7 @@ const TicketDetails = () => {
   const theme = useTheme({
     Table: `
         --data-table-library_grid-template-columns:  repeat(17, 200px);
-        max-height: 841px;
+        max-height: 500px;
       `,
     BaseCell: `
         &:nth-of-type(1) {
@@ -61,7 +61,7 @@ const TicketDetails = () => {
       <div className="dash_content_main oflow-hd">
         <div className="ticketDatashowMore_head oflow-hd">
           <div className="backbtnwithtxt oflow-hd">
-            <Link to="/ticketing">
+            <Link to="/event-ticketing-registration">
               <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M8.50004 12C8.4284 12.0001 8.35745 11.986 8.29127 11.9586C8.22509 11.9311 8.16499 11.8909 8.1144 11.8402L2.65987 6.38565C2.60919 6.33503 2.56898 6.27492 2.54155 6.20875C2.51412 6.14258 2.5 6.07165 2.5 6.00002C2.5 5.92839 2.51412 5.85746 2.54155 5.79129C2.56898 5.72512 2.60919 5.665 2.65987 5.61438L8.1144 0.159852C8.32754 -0.0532839 8.67267 -0.0532839 8.88567 0.159852C9.09867 0.372988 9.09881 0.718123 8.88567 0.931122L3.81678 6.00002L8.88567 11.0689C8.93635 11.1195 8.97656 11.1796 9.00399 11.2458C9.03142 11.312 9.04554 11.3829 9.04554 11.4545C9.04554 11.5262 9.03142 11.5971 9.00399 11.6633C8.97656 11.7294 8.93635 11.7896 8.88567 11.8402C8.83509 11.8909 8.77498 11.9311 8.7088 11.9586C8.64262 11.986 8.57167 12.0001 8.50004 12Z"
@@ -102,7 +102,7 @@ const TicketDetails = () => {
                     <input className="calendar" type="text" value="" placeholder="Date" />
                     <input className="time" type="text" value="" placeholder="Time" />
                   </div>
-                  <span className="searchrslttotal">Total Filtered Result: 50</span>
+                  {/* <span className="searchrslttotal">Total Filtered Result: 50</span> */}
                 </div>
               </div>
 
@@ -123,7 +123,7 @@ const TicketDetails = () => {
                             <HeaderCell>Day</HeaderCell>
                             <HeaderCell>Type</HeaderCell>
                             <HeaderCell>Quantity</HeaderCell>
-                            <HeaderCell>Purchased by - Self/Name of the buyer(Fahim)</HeaderCell>
+                            <HeaderCell>Purchased by</HeaderCell>
                             <HeaderCell>Discount</HeaderCell>
                             <HeaderCell>Discount type</HeaderCell>
                             <HeaderCell>Discount Amount</HeaderCell>
@@ -162,6 +162,19 @@ const TicketDetails = () => {
                       </>
                     )}
                   </Table>
+                  <div className="large_table_bottom oflow-hd">
+                    <div className="large_table_bottom_sr oflow-hd">
+                      Total Filtered Result: <span>50</span>
+                    </div>
+                    <ul>
+                      <li className="large_table_bottom_total_discount">
+                        Total Discount Amount: <span>1,00,000</span>
+                      </li>
+                      <li className="large_table_bottom_total_earn">
+                        Total Amount: <span>1,00,000</span>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
